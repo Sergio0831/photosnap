6382334b18312b271a69aa5b30e571eee4a2ecbb
Sergio0831 <sergejs.ivcenko@gmail.com>
2022-02-05 08:34:44 +0000

    Implemented pricin cards functionality

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -9,7 +9,8 @@ type PricingCardProps = {
 };
 
 const PricingCard = ({ card }: PricingCardProps) => {
-  const { plan } = useAppContext();
+  const { plan, isOn } = useAppContext();
+  const index = isOn ? 1 : 0;
   const cardClasses = clsx({
     [classes.card]: true,
     [classes.card__popular]: card.popular
@@ -17,10 +18,15 @@ const PricingCard = ({ card }: PricingCardProps) => {
 
   return (
     <div className={cardClasses}>
-      <div className={classes.card__text}></div>
-      <h3>{card.title}</h3>
-      <p>{card.description}</p>
-      <div className={classes.card__price}>{/* <h2>${card.price}</h2> */}</div>
+      <div className={classes.card__text}>
+        {" "}
+        <h3>{card.title}</h3>
+        <p>{card.description}</p>
+      </div>
+      <div className={classes.card__price}>
+        <h2>{card.price[index].amount}</h2>
+        <span>{plan === "monthly" ? "per month" : "per year"}</span>
+      </div>
       <Button
         type='button'
         className={card.popular ? "btn-fill--light" : "btn-fill--dark"}
